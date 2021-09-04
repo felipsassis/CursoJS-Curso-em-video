f00ef4c30fc1273b9b3cb9634566031aeaf2257f
felipsassis <79009415+felipsassis@users.noreply.github.com>
2021-09-04 11:08:25 -0300

    tabuada com for

---
+++ Modulo-E/Aula-2/js/script.js
@@ -0,0 +1,11 @@
+function calcular(){
+    var num = document.getElementById("numero").value
+    var res = document.getElementById("res")
+    res.innerHTML = `<p>COMPLETO!</p><br>`
+
+    for (contador = 1; contador<=10; contador++){
+        res.innerHTML += `<p> ${num} X ${contador} = ${parseInt(num)*contador}</p>`
+    }
+
+
+
